1975f94ff7ac0d0011804678b62384ff09ccafa8
Evan Lawrence-Hurt <evanlh@gmail.com>
2015-04-01 10:30:42 -0400

    upgrade mongodb drivers, remove sort, add logging code to help troubleshoot

--- example/publish.js
+++ example/publish.js
@@ -3,11 +3,15 @@ var mubsub = require('../lib/index');
 var client = mubsub(process.env.MONGODB_URI || 'mongodb://localhost:27017/mubsub_example');
 var channel = client.channel('example');
 
+client.on('error', console.error);
 channel.on('error', console.error);
-client.on('error', console.error)
 
 setInterval(function () {
+    console.log("trying!");
     channel.publish('foo', { foo: 'bar', time: Date.now() }, function (err) {
-        if (err) throw err;
+        if (err) {
+          console.log(err.stack || err.toString());
+          throw err;
+        }
     });
 }, 2000);
--- example/subscribe.js
+++ example/subscribe.js
@@ -2,9 +2,13 @@ var mubsub = require('../lib/index');
 
 var client = mubsub(process.env.MONGODB_URI || 'mongodb://localhost:27017/mubsub_example');
 var channel = client.channel('example');
+var handleError = function(err){
+  console.error(err);
+  console.error(err.stack);
+};
 
-channel.on('error', console.error);
-client.on('error', console.error);
+channel.on('error', handleError);
+client.on('error', handleError);
 
 channel.subscribe('foo', function (message) {
     console.log(message);
